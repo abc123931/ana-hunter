38d3d8037c1c322f4bfe86f541c7fca51409fc4f
abc123931 <abc123931@gmail.com>
2020-05-11 22:08:19 +0900

    :wrench: Adding config files.

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    es6: true,
+    jest: true,
+    node: true,
+  },
+
+  parser: "@typescript-eslint/parser",
+
+  parserOptions: {
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+
+  plugins: ["@typescript-eslint", "react", "react-hooks", "prettier"],
+
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+
+  rules: {
+    "no-irregular-whitespace": "off",
+    "react/display-name": "off",
+    "react/prop-types": "off",
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
+
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
