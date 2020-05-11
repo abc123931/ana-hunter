2bb9686cc083a40e3e25eb710c58dac665e4bc37
abc123931 <abc123931@gmail.com>
2020-05-12 00:08:53 +0900

    :label: Adding stack navigation types.

---
+++ navigation/types.ts
@@ -0,0 +1,15 @@
+export type HomeStackParamList = {
+  Home: undefined;
+};
+
+export type BlogStackParamList = {
+  Blog: undefined;
+};
+
+export type HorseStackParamList = {
+  Horse: undefined;
+};
+
+export type RaceStackParamList = {
+  Race: undefined;
+};
