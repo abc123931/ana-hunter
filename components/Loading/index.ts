a8000dc71296af29e7827670e4bdccea69bcf947
abc123931 <abc123931@gmail.com>
2020-05-18 00:32:02 +0900

    :sparkles: Adding Blog Loading.

--- components/Loading/index.ts
+++ components/Loading/index.ts
@@ -1,4 +1,5 @@
 import HomeRaceCardLoading from "./Home/RaceCardLoadingList";
 import TableLoading from "./Table/TableLoading";
+import BlogLoading from "./Blog/BlogLoadingList";
 
-export { HomeRaceCardLoading, TableLoading };
+export { HomeRaceCardLoading, TableLoading, BlogLoading };
