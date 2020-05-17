9a680b7b1e34873dd9a10e945f0a59550102a51a
abc123931 <abc123931@gmail.com>
2020-05-18 00:32:22 +0900

    :sparkles: Adding Refresh control.

--- components/Home/RaceList/RaceList.tsx
+++ components/Home/RaceList/RaceList.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { StyleSheet, Text, FlatList } from "react-native";
+import React, { useCallback } from "react";
+import { Text, ScrollView, RefreshControl } from "react-native";
 import { useGetWeekendRacesQuery, GetWeekendRacesQuery } from "../../../apollo/graphql";
 import { RaceCard } from "./RaceCard";
 import { HomeRaceCardLoading } from "../../Loading";
@@ -7,28 +7,31 @@ import { HomeRaceCardLoading } from "../../Loading";
 type RecieveProps = {};
 type ContainerCreatedProps = {
   races: GetWeekendRacesQuery["races"];
+  refreshing: boolean;
+  onRefresh: () => void;
 };
 type Props = Omit<RecieveProps & ContainerCreatedProps, "">;
 
-const Component: React.FC<Props> = ({ races, ..._props }) => (
-  <FlatList
-    style={styles.container}
-    data={races}
-    renderItem={({ item }) => <RaceCard data={item} />}
-    keyExtractor={(_item, index) => `${index}`}
-  />
+const Component: React.FC<Props> = ({ races, refreshing, onRefresh, ..._props }) => (
+  <ScrollView refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+    {races.map((race, index) => (
+      <RaceCard key={index} data={race} />
+    ))}
+  </ScrollView>
 );
 
-const styles = StyleSheet.create({
-  container: {},
-});
-
 const Container: React.FC<RecieveProps> = ({ ...props }) => {
-  const { data, loading, error } = useGetWeekendRacesQuery();
+  const [refreshing, setRefreshing] = React.useState<boolean>(false);
+  const { data, loading, error, refetch } = useGetWeekendRacesQuery();
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  }, [refetch]);
   if (loading) return <HomeRaceCardLoading />;
   if (error) return <Text>エラーが発生しました。</Text>;
   if (!data || data?.races.length === 0) return <Text>レースが見つかりませんでした。</Text>;
-  return <Component races={data.races} {...props} />;
+  return <Component races={data.races} refreshing={refreshing} onRefresh={onRefresh} {...props} />;
 };
 
 export default Container;
