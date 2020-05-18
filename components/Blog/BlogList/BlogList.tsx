25daed0850badf352cb3695ff3644e0bdf46eb2c
abc123931 <abc123931@gmail.com>
2020-05-18 09:20:26 +0900

    :sparkles: Adding refresh control.

--- components/Blog/BlogList/BlogList.tsx
+++ components/Blog/BlogList/BlogList.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Text, FlatList } from "react-native";
+import React, { useCallback } from "react";
+import { Text, ScrollView, RefreshControl } from "react-native";
 import { useGetBlogsQuery, GetBlogsQuery } from "../../../apollo/graphql";
 import { BlogCard } from "./BlogCard";
 import { BlogLoading } from "../../Loading";
@@ -7,25 +7,33 @@ import { BlogLoading } from "../../Loading";
 type RecieveProps = {};
 type ContainerCreatedProps = {
   blogs: GetBlogsQuery["blogs"];
+  refreshing: boolean;
+  onRefresh: () => void;
 };
 type Props = Omit<RecieveProps & ContainerCreatedProps, "">;
 
-const Component: React.FC<Props> = ({ blogs, ..._props }) => (
-  <FlatList
-    data={blogs}
-    renderItem={({ item }) => <BlogCard blog={item} />}
-    keyExtractor={(_item, index) => `${index}`}
-  />
+const Component: React.FC<Props> = ({ blogs, refreshing, onRefresh, ..._props }) => (
+  <ScrollView refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+    {blogs.map((blog, index) => (
+      <BlogCard key={index} blog={blog} />
+    ))}
+  </ScrollView>
 );
 
 const Container: React.FC<RecieveProps> = ({ ...props }) => {
-  const { data, loading, error } = useGetBlogsQuery();
+  const [refreshing, setRefreshing] = React.useState<boolean>(false);
+  const { data, loading, error, refetch } = useGetBlogsQuery();
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  }, [refetch]);
 
   if (loading) return <BlogLoading />;
   if (error || !data) return <Text>エラーが発生しました</Text>;
   if (!data.blogs || data.blogs.length === 0) return <Text>ブログがありませんでした</Text>;
 
-  return <Component blogs={data.blogs} {...props} />;
+  return <Component blogs={data.blogs} refreshing={refreshing} onRefresh={onRefresh} {...props} />;
 };
 
 export default Container;
